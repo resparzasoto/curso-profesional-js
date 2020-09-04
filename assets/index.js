039283698d57e8f48dbe3a3e1d94267fc520a377
resparzasoto <rubenesparzasoto95@gmail.com>
2020-09-04 00:17:24 -0600

    Cómo llega un script al navegador

---
+++ assets/index.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const video = document.querySelector('video')
+const button = document.querySelector('button')
+
+function MediaPlayer (config) {
+  this.media = config.el
+}
+
+MediaPlayer.prototype.play = function () {
+  this.media.play()
+}
+
+MediaPlayer.prototype.pause = function () {
+  this.media.pause()
+}
+
+MediaPlayer.prototype.togglePlay = function () {
+  this.media.paused ? this.media.play() : this.media.pause()
+}
+
+const player = new MediaPlayer({ el: video })
+button.onclick = () => player.togglePlay()
