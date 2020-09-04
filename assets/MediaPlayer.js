a35e9a2a5ecf07bb8ead4a36dcdd6cf65c519c90
resparzasoto <rubenesparzasoto95@gmail.com>
2020-09-04 07:46:11 -0600

    Scope

---
+++ assets/MediaPlayer.js
@@ -0,0 +1,19 @@
+'use strict'
+
+function MediaPlayer (config) {
+  this.media = config.el
+}
+
+MediaPlayer.prototype.play = function () {
+  this.media.play()
+}
+
+MediaPlayer.prototype.pause = function () {
+  this.media.pause()
+}
+
+MediaPlayer.prototype.togglePlay = function () {
+  this.media.paused ? this.media.play() : this.media.pause()
+}
+
+export default MediaPlayer
--- assets/index.js
+++ assets/index.js
@@ -1,23 +1,9 @@
 'use strict'
 
+import MediaPlayer from './MediaPlayer.js'
+
 const video = document.querySelector('video')
 const button = document.querySelector('button')
-
-function MediaPlayer (config) {
-  this.media = config.el
-}
-
-MediaPlayer.prototype.play = function () {
-  this.media.play()
-}
-
-MediaPlayer.prototype.pause = function () {
-  this.media.pause()
-}
-
-MediaPlayer.prototype.togglePlay = function () {
-  this.media.paused ? this.media.play() : this.media.pause()
-}
 
 const player = new MediaPlayer({ el: video })
 button.onclick = () => player.togglePlay()
