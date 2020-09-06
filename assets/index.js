e87c762e23b82c908896fb2b5fa8b89363ca735e
resparzasoto <rubenesparzasoto95@gmail.com>
2020-09-05 18:05:55 -0600

    this

--- assets/index.js
+++ assets/index.js
@@ -1,13 +1,18 @@
 'use strict'
 
 import MediaPlayer from './MediaPlayer.js'
-import AutoPlay from './plugins/AutoPlay.js'
+// import AutoPlay from './plugins/AutoPlay.js'
 
 const video = document.querySelector('video')
 const btnPlayPause = document.getElementById('btn-play-pause')
 const btnMuteUnmute = document.getElementById('btn-mute-unmute')
 
-const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] })
+const player = new MediaPlayer({
+  el: video,
+  plugins: [
+    // new AutoPlay()
+  ]
+})
 
 btnPlayPause.onclick = () => player.togglePlay()
 btnMuteUnmute.onclick = () => player.toggleMute()
