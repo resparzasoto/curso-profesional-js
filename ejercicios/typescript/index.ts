5d902f91e2a558e6cbf19d4e2ba0c767eced6369
resparzasoto <rubenesparzasoto95@gmail.com>
2020-09-06 20:13:27 -0600

    Tipos básicos

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -1,9 +1,50 @@
-'use strict'
+"use strict";
 
-console.log('Hello, TypeScript');
+console.log("Hello, TypeScript");
 
 function add(a: number, b: number) {
-    return a + b;
+  return a + b;
 }
 
 const sum = add(2, 3);
+
+//Boolean
+let muted: boolean = true;
+muted = false;
+
+// Números
+let numerador: number = 42;
+let denominador: number = 6;
+let resultado = numerador / denominador;
+
+// String
+let nombre: string = "Richard";
+let saludo = `Me llamo ${nombre}`;
+
+// Arreglos
+let people: string[] = [];
+people = ["Isabel", "Nicole", "Raul"];
+// people.push(9000);
+
+// Arreglos con diferentes tipos
+// Puede tener strings o números
+let peopleAndNumbers: Array<string | number> = [];
+
+// Enum
+enum Color {
+  Red = "Rojo",
+  Green = "Verde",
+  Blue = "Azul",
+  Yellow = "Amarillo",
+}
+
+let colorFavorito: Color = Color.Yellow;
+
+console.log(`Mi color favorito es ${colorFavorito}`);
+
+// Any
+let comodin: any = "Joker";
+comodin = { type: "Wildcard" };
+
+// Object
+let someObject: object = { type: "Wildcard" };
